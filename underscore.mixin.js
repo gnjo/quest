cb536b59148a7e4a080c0242374a60f7aa237edc
gunjyo <35440828+gnjo@users.noreply.github.com>
2019-09-03 17:03:50 +0900

    Create underscore.mixin.js

---
+++ underscore.mixin.js
@@ -0,0 +1,41 @@
+/*history
+v0.1 isExist
+v0.2 randomSeed
+*/
+
+_.mixin({
+ isExist : function(obj) {
+  if(obj===0||obj==='0')return true;
+  return _.isEmpty(obj)
+ }
+})
+/*usage
+_isExist(0) //exist
+*/
+;
+_.mixin({
+  randomSeed : function entry(_seed){
+ let seed=_seed||999
+ ,xorshift32=function xorshift32(seed){
+  let y=seed
+  y = y ^ (y << 13)
+  y = y ^ (y >> 17)
+  y = y ^ (y << 15)
+  y = y >>> 0
+  return y
+ }
+ ,random=(a,b)=>{
+  seed=xorshift32(seed)
+  if(_.isExist(a)&&_.isExist(b))return seed%Math.abs(b-a)+a
+  if(_.isExist(a))return seed%a
+  return seed
+ }
+ return random
+}
+})
+/*usage
+let random=_.randomSeed(999)
+document.body.textContent=random(0,5)
+*/
+;
+
