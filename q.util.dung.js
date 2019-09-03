f042cbd30256ff93b218fa7b1fc28d79b1b8ee45
gunjyo <35440828+gnjo@users.noreply.github.com>
2019-09-03 15:02:05 +0900

    Update q.util.dung.js

--- q.util.dung.js
+++ q.util.dung.js
@@ -34,39 +34,33 @@ B,3,BU7,BLA
  fn.setary=(ary,key)=>{
   return (!key)?Array.from(new Set(ary)):ary.filter((a,b,c)=>(c.findIndex((d)=>(a[key]===d[key]))=== b))
  }
-fn.crcTable=(function(){
-  var c,crcTable = [];
-  for(var n =0; n < 256; n++){
-    c = n;
-    for(var k =0; k < 8; k++){
-      c = ((c&1) ? (0xEDB88320 ^ (c >>> 1)) : (c >>> 1));
-    }
-    crcTable[n] = c;
-  }
-  return crcTable;
-})();//early gen
-fn.crc32 = function(str,hex=true) {
-  var crcTable = fn.crcTable,pad=( (d,l)=>('000000000000000000'+d).slice(-1*l))
-  ,crc = 0 ^ (-1)
-  ;
-  for (var i = 0; i < str.length; i++ ) crc = (crc >>> 8) ^ crcTable[(crc ^ str.charCodeAt(i)) & 0xFF]
-  ;
-  crc = (crc ^ (-1)) >>> 0
-  ;
-  return (hex)?pad(crc.toString(16),8):crc
-} 
- 
-  ;(function(root){
+/*history
+v1.0 crc
+v1.1 logic change xorshift
+*/
+;(function(root){
+
+ function xorshift32(seed){
+  let y=seed
+  y = y ^ (y << 13)
+  y = y ^ (y >> 17)
+  y = y ^ (y << 15)
+  y = y >>> 0
+  return y
+ }
+
  function entry(_seed){
   let seed=_seed||999
   ,rand=(a,b)=>{
-   seed=fn.crc32(seed.toString(),false)
-   return (a&&b)?(seed%Math.abs(b-a)+a):(a)?seed%a:seed
+   seed=xorshift32(seed)
+   return ( (a!=null)&&(b!=null) )?(seed%Math.abs(b-a)+a):(a!=null)?seed%a:seed
   }
   return rand
  }
- root.Rand=entry
+ //root.rand=entry
+ root.Rand=entry //special
 })(this);
+ 
  
  function roomer(_seed,_w,_h,str){
   let chspace=str.charAt(0)
