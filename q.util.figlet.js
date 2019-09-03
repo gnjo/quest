81a1b2f6bd9e2b3e1585743b36df7f5a807320fe
gunjyo <35440828+gnjo@users.noreply.github.com>
2019-09-03 11:26:12 +0900

    Create q.util.figlet.js

---
+++ q.util.figlet.js
@@ -0,0 +1,123 @@
+/*need font
+v1.0 figlet migrate
+*/
+
+/**
+ * Figlet JS
+ * 
+ * Copyright (c) 2010 Scott González
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ * 
+ * http://github.com/scottgonzalez/figlet-js
+ */
+
+(function() {
+
+var Figlet = (typeof exports !== "undefined" ? exports : window).Figlet = {
+	fonts: {},
+	
+	parseFont: function(name, fn) {
+		if (name in Figlet.fonts) {
+			fn();
+			return;
+		}
+		
+		Figlet.loadFont(name, function(defn) {
+			Figlet._parseFont(name, defn, fn);
+		});
+	},
+	
+	_parseFont: function(name, defn, fn) {
+		var lines = defn.split("\n"),
+			header = lines[0].split(" "),
+			hardblank = header[0].charAt(header[0].length - 1),
+			height = +header[1],
+			comments = +header[5];
+		
+		Figlet.fonts[name] = {
+			defn: lines.slice(comments + 1),
+			hardblank: hardblank,
+			height: height,
+			char: {}
+		};
+		fn();
+	},
+	
+	parseChar: function(char, font) {
+		var fontDefn = Figlet.fonts[font];
+		if (char in fontDefn.char) {
+			return fontDefn.char[char];
+		}
+		
+		var height = fontDefn.height,
+			start = (char - 32) * height,
+			charDefn = [],
+			i;
+		for (i = 0; i < height; i++) {
+			charDefn[i] = fontDefn.defn[start + i]
+				.replace(/@/g, "")
+				.replace(RegExp("\\" + fontDefn.hardblank, "g"), " ");
+		}
+		return fontDefn.char[char] = charDefn;
+	},
+
+	write: function(str, font, fn) {
+		Figlet.parseFont(font, function() {
+			var chars = [],
+				result = "";
+			for (var i = 0, len = str.length; i < len; i++) {
+				chars[i] = Figlet.parseChar(str.charCodeAt(i), font);
+			}
+			for (i = 0, height = chars[0].length; i < height; i++) {
+				for (var j = 0; j < len; j++) {
+					result += chars[j][i];
+				}
+				result += "\n";
+			}
+			fn(result);
+		});
+	}
+};
+
+})();
+
+;(function(root){
+//migration
+Figlet.loadFont = function(name, fn) {
+ let base="http://gnjo.github.io/figlet/"
+ let url=base+"fonts/" + name + ".flf"
+ fetch(url).then(d=>d.text()).then(fn)
+};
+Figlet.loadFont('roman2', function(defn) {
+ Figlet._parseFont('roman2', defn, ()=>{return/**/});
+});
+
+Figlet.write=	function(str, _font, _fn) {
+
+ let fn=_fn
+ let font=_font
+ if(!fn){
+  fn=_font
+  font='roman2'
+ }
+
+ Figlet.parseFont(font, function() {
+  var chars = [],
+      result = "";
+  for (var i = 0, len = str.length; i < len; i++) {
+   chars[i] = Figlet.parseChar(str.charCodeAt(i), font);
+  }
+  for (i = 0, height = chars[0].length; i < height; i++) {
+   for (var j = 0; j < len; j++) {
+    result += chars[j][i];
+   }
+   result += "\n";
+  }
+  fn(result);
+ });
+}
+
+
+})(this);
+
